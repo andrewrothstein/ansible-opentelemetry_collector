800b3cd402a95d199630c46a1fe9bd2979e40cf1
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-05-02 15:26:42 -0400

    latest CI, deps, meta. otelcol:0.50.0

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -1,43 +1,49 @@
 #!/usr/bin/env sh
 set -e
 DIR=~/Downloads
-MIRROR=https://github.com/open-telemetry/opentelemetry-collector/releases/download
-
-# https://github.com/open-telemetry/opentelemetry-collector/releases/download/v0.33.0/otelcol_linux_amd64
+MIRROR=https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download
 
 dl() {
     local ver=$1
     local app=$2
-    local lchecksums=$3
-    local os=$4
-    local arch=$5
-    local dotexe=${6:-}
+    local os=$3
+    local arch=$4
+    local archive_type=${5:-tar.gz}
     local platform="${os}_${arch}"
-    local file="${app}_${platform}${dotexe}"
+    local file="${app}_${ver}_${platform}.${archive_type}"
     local url="$MIRROR/v$ver/$file"
+    local lfile="${DIR}/${file}"
+    if [ ! -e $lfile ];
+    then
+        curl -sSLf -o $lfile $url
+    fi
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
 }
 
 dl_ver() {
     local ver=$1
-    local app=$2
-    # https://github.com/open-telemetry/opentelemetry-collector/releases/download/v0.33.0/checksums.txt
-    local url="$MIRROR/v$ver/checksums.txt"
-    local lchecksums="$DIR/${app}_${ver}_checksums.txt"
-    if [ ! -e $lchecksums ];
-    then
-        wget -q -O $lchecksums $url
-    fi
+    local app=${2:-otelcol}
 
-    printf "  # %s\n" $url
     printf "  '%s':\n" $ver
 
-    dl $ver $app $lchecksums darwin amd64
-    dl $ver $app $lchecksums darwin arm64
-    dl $ver $app $lchecksums linux amd64
-    dl $ver $app $lchecksums linux arm64
-    dl $ver $app $lchecksums windows amd64 .exe
+    dl $ver $app darwin amd64
+    dl $ver $app darwin arm64
+    dl $ver $app linux 386
+    dl $ver $app linux amd64
+    dl $ver $app linux arm64
+    dl $ver $app windows 386
+    dl $ver $app windows amd64
 }
 
-dl_ver ${1:-0.33.0} otelcol
+dl_ver 0.40.0
+dl_ver 0.41.0
+dl_ver 0.42.0
+dl_ver 0.43.0
+dl_ver 0.44.0
+dl_ver 0.45.0
+dl_ver 0.46.0
+dl_ver 0.47.0
+dl_ver 0.48.0
+dl_ver 0.49.0
+dl_ver ${1:-0.50.0}
