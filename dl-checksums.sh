07faa6e7eac88529ccb67129570156ae3266483f
Andrew Rothstein <andrew.rothstein@gmail.com>
2024-06-03 21:56:38 +0000

    otel(-contrib):0.101.0, delint, drop prior version

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -13,19 +13,28 @@ dl() {
     local platform="${os}_${arch}"
     local file="${app}_${ver}_${platform}.${archive_type}"
     local url="$MIRROR/v$ver/$file"
-    local shasum=$(grep $file $lchecksums | awk '{print $1}')
-    if [ ! -z $shasum ];
+    local shasum=$(egrep -e "$file\$" $lchecksums | awk '{print $1}')
+    if [ ! -z "$shasum" ];
     then
-        printf "        # %s\n" $url
-        printf "        %s: sha256:%s\n" $platform $shasum
+        printf "      # %s\n" $url
+        printf "      %s: sha256:%s\n" "$platform" "$shasum"
     fi
 }
 
 dlappvers() {
-    local lchecksums=$1
-    local ver=$2
-    local app=$3
+    local ver=$1
+    local app=$2
 
+    # https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.101.0/opentelemetry-collector-releases_otelcol_checksums.txt
+    # https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.101.0/opentelemetry-collector-releases_otelcol-contrib_checksums.txt
+    local lchecksums="${DIR}/opentelemetry-collector-releases_${app}_checksums_${ver}.txt"
+    local url="${MIRROR}/v${ver}/opentelemetry-collector-releases_${app}_checksums.txt"
+    if [ ! -e $lchecksums ];
+    then
+        curl -sSLf -o $lchecksums $url
+    fi
+
+    printf "    # %s\n" $url
     printf "    %s:\n" $app
     dl $lchecksums $ver $app darwin amd64
     dl $lchecksums $ver $app darwin arm64
@@ -38,20 +47,9 @@ dlappvers() {
 
 dl_ver() {
     local ver=$1
-
-    local lchecksums=$DIR/opentelemetry-collector-releases_checksums_${ver}.txt
-    # https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.50.0/opentelemetry-collector-releases_checksums.txt
-    local url=$MIRROR/v${ver}/opentelemetry-collector-releases_checksums.txt
-    if [ ! -e $lchecksums ];
-    then
-        curl -sSLf -o $lchecksums $url
-    fi
-
-    printf "  # %s\n" $url
     printf "  '%s':\n" $ver
-
-    dlappvers $lchecksums $ver otelcol
-    dlappvers $lchecksums $ver otelcol-contrib
+    dlappvers $ver otelcol
+    dlappvers $ver otelcol-contrib
 }
 
-dl_ver ${1:-0.74.0}
+dl_ver ${1:-0.101.0}
